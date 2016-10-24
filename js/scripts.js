5f7bb6efde54786550ac58a6a54a939fc0496664
Erica Nafziger and Luke Philips <luke@>
2016-10-24 15:51:19 -0700

    use regex to remove punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    // var sentenceString = $("input#sentence").val();
-    var sentenceString = "This is a sentence, like any other.";
-    var sentenceArray = sentenceString.split(" ");
+    var sentenceString = $("input#sentence").val();
+    // var sentenceString = "This is a sentence, like any other.";
+    var lowerCaseString = sentenceString.toLowerCase();
+    var punctuationless = lowerCaseString.replace(/[.,]/g,"");
+    var finalString = punctuationless.replace(/\s{2,}/g," ");
+
+    var sentenceArray = finalString.split(" ");
     var newArray = [];
+    var newString ="";
     sentenceArray.forEach(function(word){
         var length = word.length;
       if (length >= 3) {
         newArray.push(word);
       }
+
     });
+    newString = newArray.reverse().join([separator = ' ']);
+    $("#newSentence").text(newString);
     // sentenceArray.push()
     console.log(sentenceString);
     console.log(sentenceArray);
